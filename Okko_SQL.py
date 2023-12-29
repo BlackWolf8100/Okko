320cb06a74194b56a52a8dee78c2be7382dbfb00
Mykola Manenko <manenkogena847@gmail.com>
2023-12-29 11:22:38 +0200

    fixed

--- Okko_SQL.py
+++ Okko_SQL.py
@@ -31,7 +31,7 @@ def loader(url, LOGIN, PASSWORD):
     #    f.write(str(data))
 
     print(len(data))
-    return date
+    return data
 
     
 def test2():
@@ -41,16 +41,16 @@ def test2():
 
 def main():
 
-    # test1()
-    # die
-
     db = My_base()
     db.open()
     
     today = datetime.today()
     date0 = datetime(2023, 11, 23)
     
-    sql = 'SELECT MAX(`trans_date`) FROM Okko_data'
+    sql = f'DELETE FROM Okko_data WHERE trans_date LIKE "{today:%d.%m.%Y}%"'
+    db.execute(sql)
+    
+    sql = 'SELECT trans_date FROM Okko_data ORDER BY trans_id DESC LIMIT 1'
     result = db.get_one_table(sql)
     print(result) 
     if result[0]:
@@ -59,11 +59,12 @@ def main():
     else:
         dt = date0
     date1 = dt + timedelta(days=1)
-    date2 = today - timedelta(days=-300)
+    date2 = today
     print(date1)
     print(date2)
     
-    if date1 < date2:
+    
+    if date1 <= date2:
         url = f'{BASE_API}/transactions?date_from={date1:%Y-%m-%d}&date_to={date2:%Y-%m-%d}&processed_in_bo=true'
         print(url)
         result = loader(url, db.cfg['LOGIN'], db.cfg['PASSWORD'])
